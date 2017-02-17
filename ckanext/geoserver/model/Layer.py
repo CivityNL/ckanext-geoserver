c25e492354dd3da03df16367da007cfcc04547b6
Daniel Henzen <daniel_henzen@yahoo.de>
2017-02-17 13:08:31 +0100

    If a SLD file is provided in the resources of a dataset, it is used as style for the geoserver publishing process.

--- ckanext/geoserver/model/Layer.py
+++ ckanext/geoserver/model/Layer.py
@@ -7,13 +7,17 @@ from ckanext.geoserver.model.MultiDatastored import MultiDatastored
 from ckanext.geoserver.model.MultiShapeFile import MultiShapeFile
 from ckan.plugins import toolkit
 from pylons import config
+from lxml import etree, objectify
+from StringIO import StringIO
 import json
 import urllib
+import logging
 import re
+import ckanext.geoserver.misc.helpers as helpers
 
 class Layer(object):
     """
-    Creates an OGC:WFS and an OGC:WMS layer in Geoserver and updates the CKAN package dictionary with new resource
+    Creates an WFS and an WMS layer in Geoserver and updates the CKAN package dictionary with new resource
     information.  The class of the object instance is called first here instead of the object instance itself.  By
     calling the the class method without instantiating the class itself, we essentially create a subclass (not a
     parent class) via inheritance.
@@ -99,6 +103,7 @@ class Layer(object):
         """
         self.create_layer()
         self.create_geo_resources()
+        self.add_sld_if_exists()
 
         return True
 
@@ -265,6 +270,50 @@ class Layer(object):
             toolkit.get_action("resource_delete")(context, {"id": result["id"]})
 
         return True
+
+    def add_sld_if_exists(self):
+        """
+        Checks if SLD was uploaded.
+        Validates SLD according to SLD Standard 1.0.0
+        Make some minor changes to SLD (lowercase columnnames, rename geom column to wkb_geometry)
+        Add SLD to geoserver
+        Add SLD to this layer
+
+        @returns True if successful
+        """
+
+        for resource in toolkit.get_action("package_show")(None, {"id": self.package_id}).get('resources', []):
+            # use first found sld file as style
+            if resource.get("format", {}).lower() == "sld":
+                url = resource.get("url", {})
+                xml_file = open(helpers.file_path_from_url_shp(url), 'r')
+                xml_string = xml_file.read()
+                xml_file.close()
+                # validate sld according to xsd
+                if (self.xml_validator(xml_string)):
+                    #do some xml cleaning
+                    #   lowercase all table names
+                    cleaned_xml =re.sub("(<[a-zA-Z]*:PropertyName>)(\w*)(<\/[a-zA-Z]*:PropertyName>)", lambda match:match.group(1)+""+match.group(2).lower()+""+match.group(3), xml_string)
+                    #   replace geom column name with "wkb_geometry"
+                    cleaned_xml = re.sub("(<[a-zA-Z]*:?Geometry>\s*<[a-zA-Z]*:?PropertyName>)(\w*)(<\/[a-zA-Z]*:?PropertyName>\s*<\/[a-zA-Z]*:?Geometry>)", lambda match: match.group(1)+"wkb_geometry"+match.group(3), cleaned_xml)
+
+                    #add sld to geoserver
+                    self.geoserver.create_style(self.getName(), cleaned_xml, overwrite=True)
+                    # connect sld to layer
+                    layer = self.geoserver.get_layer(self.store.workspace.name+":"+self.getName())
+
+                    layer._set_default_style(self.getName())
+
+                break
+
+
+    def xml_validator(self, some_xml_string, xsd_file='http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd'):
+        xsd = urllib.urlopen(xsd_file)
+        xsd_string = xsd.read()
+        schema = etree.XMLSchema(etree.parse(StringIO(xsd_string)))
+        doc = etree.parse(StringIO(some_xml_string))
+        return schema.validate(doc)
+
 
     def getName(self):
         if self.resource_id == 'schema_descriptor_multi' or self.resource_id == 'shapefile_multi' :
